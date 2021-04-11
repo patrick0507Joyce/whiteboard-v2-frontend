aeab74727479c47aeca72c6e1abea0ab760a8132
Xiaohu Shu <sxh20150609@gmail.com>
2021-04-11 16:48:24 +0300

    frontend

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const ENDPOINT = 'localhost:9999';
+const ENDPOINT = 'https://whiteboard-v2.herokuapp.com/';
 
 let socket = io(ENDPOINT, { transports: ['polling', 'websocket'] });
 
